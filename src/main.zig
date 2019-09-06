1ec21a9830fce2dc1ee2f870479bfeb443c8e91a
Rickard Andersson <gonz@severnatazvezda.com>
2019-09-06 11:23:52 +0300

    add `MAKEINTRESOURCEA` & `MAKEINTRESOURCEW`
    weird macros not included properly otherwise

--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,11 @@
+const windows = @import("std").os.windows;
 pub const c = @import("./win32/c.zig");
 pub const lean_and_mean = @import("./win32/lean_and_mean.zig");
+
+pub fn MAKEINTRESOURCEA(value: windows.INT) windows.LPSTR {
+    return @ptrCast(windows.LPSTR, @intToPtr(*windows.ULONG_PTR, @intCast(windows.WORD, value)));
+}
+
+pub fn MAKEINTRESOURCEW(value: windows.INT) windows.LPSTR {
+    return @ptrCast(windows.LPWSTR, @intToPtr(*windows.ULONG_PTR, @intCast(windows.WORD, value)));
+}
