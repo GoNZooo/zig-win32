423cf7f7765c390dd9a1e1e1666d445bfd085b4f
Rickard Andersson <gonz@severnatazvezda.com>
2020-04-07 21:48:11 +0300

    add test to build script

--- build.zig
+++ build.zig
@@ -5,4 +5,10 @@ pub fn build(b: *Builder) void {
     const lib = b.addStaticLibrary("win32", "src/main.zig");
     lib.setBuildMode(mode);
     lib.install();
+
+    var tests = b.addTest("src/main.zig");
+    tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&tests.step);
 }
